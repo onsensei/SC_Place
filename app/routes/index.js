4975bb5e3468e82d210630b07e448cec9fd6c266
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-02-23 16:32:09 +0700

    use test page as main page in route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,14 @@
-import Home from '../pages/Home/Home.page';
+// import Home from '../pages/Home/Home.page';
+import Place from '../pages/Place/Place.page';
+import Test from '../pages/Test/Test.page';
 import {createAppContainer, createStackNavigator} from 'react-navigation';
 
 const RootNavigator = createStackNavigator({
-  Home: {
-    screen: Home
+  Test: {
+    screen: Test
+  },
+  Place: {
+    screen: Place
   }
 });
 
