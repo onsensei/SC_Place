2910c68480f16905c1039feb935855dbe33ba828
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-02-23 16:33:09 +0700

    add function getNumberOfSequenceWithIndex

--- app/pages/Test/Test.page.js
+++ app/pages/Test/Test.page.js
@@ -4,7 +4,21 @@ import Test from '../../components/Test/Test.component';
 import {connect} from 'react-redux';
 
 class TestPage extends Component {
+
+  getNumberOfSequenceWithIndex = (x) => {
+    if (x <= 0) {
+      return 3;
+    } else {
+      return 2 * x + this.getNumberOfSequenceWithIndex(x - 1);
+    }
+  }
+
   render () {
+    // console.log(this.getNumberOfSequenceWithIndex(0)); // 3
+    // console.log(this.getNumberOfSequenceWithIndex(1)); // 5
+    // console.log(this.getNumberOfSequenceWithIndex(2)); // 9
+    // console.log(this.getNumberOfSequenceWithIndex(3)); // 15
+
     return (
       <Test style={styles.container}/>
     );
