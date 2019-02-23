516fac587a9c0aa27d54a13140f6b66d42e9cd47
Thanaphat Suwannikornkul <onsensei@gmail.com>
2019-02-23 23:09:21 +0700

    request places and render

--- app/components/Place/Place.component.js
+++ app/components/Place/Place.component.js
@@ -1,11 +1,47 @@
 import React, {Component} from 'react';
+import request from '../../uitls/Request.util';
 import styles from './Place.style';
-import {View} from 'react-native';
+import {FlatList, Text, View} from 'react-native';
+import {get} from 'lodash';
+import {getRestaurantInBangsueArea} from '../../configs/Request.config';
 
 export default class Place extends Component {
+
+  state = {
+    places: []
+  }
+
+  async componentDidMount () {
+    const apiConfig = getRestaurantInBangsueArea();
+    const response = await request(apiConfig);
+    const status = get(response, 'body.status');
+    if (status === 'OK') {
+      const places = get(response, 'body.results');
+      // eslint-disable-next-line
+      this.setState({
+        places
+      });
+    }
+  }
+
+  _keyExtractor = (item) => item.id;
+
+  _renderItem = ({item}) => (
+    <View>
+      <Text>{item.name}</Text>
+      <Text>{`${item.formatted_address}\n`}</Text>
+    </View>
+  )
+
   render () {
     return (
-      <View style={styles.container}/>
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.places}
+          keyExtractor={this._keyExtractor}
+          renderItem={this._renderItem}
+        />
+      </View>
     );
   }
 }
